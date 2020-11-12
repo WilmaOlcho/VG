2e816b0d4e750e121ee445b38824437782e22c52
WilmaOlcho <WilmaOlcho@gmail.com>
2020-11-12 11:50:05 +0100

    Parsing, resolving bugs, refaktoring

--- Estun.py
+++ Estun.py
@@ -3,9 +3,10 @@ import minimalmodbus
 import serial
 import configparser
 from TactWatchdog import TactWatchdog
-from PRONET_constants import PRONET_constants
+from pronet_constants import Pronet_constants
+from modbus_constants import Modbus_constants
 
-class Estun(PRONET_constants):
+class Estun(Pronet_constants, Modbus_constants):
     def __init__(self, shared, lock,
                     comport = "COM1",
                     slaveadress = 1,
@@ -17,7 +18,8 @@ class Estun(PRONET_constants):
                     parity = "N",
                     bytesize = 7,
                     *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(self)
+        #Pronet_constants.__init__(self)
         self.RTU = minimalmodbus.Instrument(comport,slaveadress,protocol,close_port_after_each_call,debug)
         self.RTU.serial.baudrate = baud
         self.RTU.serial.bytesize = bytesize 
@@ -26,19 +28,20 @@ class Estun(PRONET_constants):
         self.RTU.serial.timeout = 1
 
     def sendRegister(self, address, value):
-        return self.RTU.write_register(address,value,0,16,False)
+        return self.RTU.write_register(address,value,0,self.WRITE_REGISTER,False)
 
     def readRegister(self, address):
-        return self.RTU.read_register(address,0,3,False)
+        return self.RTU.read_register(address,0,self.READ_HOLDING_REGISTERS,False)
 
-    def parameterType(self, parameter = CurrentAlarm):
-        return parameter[-1]
+    def parameterType(self, parameter = (None,(None,None),None)):
+        return parameter[2]
 
-    def valueType(self, parameter = CurrentAlarm):
+    def valueType(self, parameter = (None,(None,None),None)):
         return parameter[1][0]
 
-    def parameterMask(self, parameter = CurrentAlarm, bitonly=False):
-        if parameterType(parameter) == 'bit' or parameterType(parameter) == 'bin' or bitonly:
+    def parameterMask(self, parameter = (None,(None,None),None), bitonly=False):
+        pType = self.valueType(parameter)
+        if pType == 'bit' or pType == 'bin' or bitonly:
             if parameter[1][1] == 0:
                 return 0b1110
             if parameter[1][1] == 1:
@@ -47,7 +50,7 @@ class Estun(PRONET_constants):
                 return 0b1011
             if parameter[1][1] == 3:
                 return 0b0111
-        elif parameterType(parameter) == "hex":
+        elif pType == "hex":
             if parameter[1][1] == 0:
                 return 0xFFF0
             if parameter[1][1] == 1:
@@ -59,33 +62,35 @@ class Estun(PRONET_constants):
         else:
             return 0
     
-    def parameterAddress(self, parameter = CurrentAlarm):
+    def parameterAddress(self, parameter = (None,(None,None),None)):
         return parameter[0]
 
     def invertedReducedMask(self, parameter):
-        mask = not parameterMask(parameter)
-        if parameterType(parameter) == 'hex':
+        pType = self.valueType(parameter)
+        pMask = self.parameterMask(parameter)
+        mask = not pMask
+        if pType == 'hex':
             mask /= 0xF
         return mask
 
     def readParameter(self, parameter):
-        if parameterType == WRITE_ONLY:
+        if self.parameterType == self.WRITE_ONLY:
             return False
         else:
-            Value = readRegister(parameterAddress)
-            if not valueType=='int':
-                Value *= invertedReducedMask(parameter)
+            Value = self.readRegister(self.parameterAddress)
+            if not self.valueType=='int':
+                Value *= self.invertedReducedMask(parameter)
             return Value
 
-    def setParameter(self, parameter = CurrentAlarm, value = 0):
-        if parameterType == READ_ONLY:
+    def setParameter(self, parameter = (None,(None,None),None), value = 0):
+        if self.parameterType == self.READ_ONLY:
             return False
         else:
-            if not valueType=='int':
-                currentValue = readRegister(parameterAddress)
-                currentValue &= parameterMask(parameter)
-                writeValue = currentValue + (value * invertedReducedMask(parameter))
-            sendRegister(parameterAddress,writeValue)
+            if not self.parameterType=='int':
+                currentValue = self.readRegister(self.parameterAddress)
+                currentValue &= self.parameterMask(parameter)
+                writeValue = currentValue + (value * self.invertedReducedMask(parameter))
+            self.sendRegister(self.parameterAddress,writeValue)
 
     @classmethod
     def Run(cls, shared, lock):
---
+++ modbus_constants.py
@@ -0,0 +1,25 @@
+
+
+
+class Modbus_constants():
+    def __init__(self, *args, **kwargs):
+        self.functionCodes = {
+            'Read Coil Status':1,
+            'Read Input Status':2,
+            'Read Holding Registers':3,
+            'Read Internal Registers':4,
+            'Force Single Coil':5,
+            'Preset Single Register':6,
+            'Force Multiple Coils':15,
+            'Preset Multiple Registers':16,
+            'Masked Write Register':22}
+        self.READ_COIL = 1
+        self.WRITE_COIL = 5
+        self.READ_INPUT = 2
+        self.WRITE_COILS = 15
+        self.WRITE_REGISTERS = 16
+        self.READ_HOLDING_REGISTERS = 3
+        self.READ_INTERNAL_HOLDING_REGISTERS = 4
+        self.WRITE_REGISTER = 6
+        self.MASKED_REGISTER = 22
+        super().__init__(self, *args, **kwargs)
