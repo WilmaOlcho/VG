2787f7a207430bb1de91c70e6c5416afc08105c3
WilmaOlcho <WilmaOlcho@gmail.com>
2020-11-24 09:08:28 +0100

    ModbusTCP units instead of ADVANTECH class

--- Sources/modbus_constants.py
+++ Sources/modbus_constants.py
@@ -7,9 +7,10 @@ class Modbus_constants():
             'Read Coil Status':1,
             'Read Input Status':2,
             'Read Holding Registers':3,
-            'Read Internal Registers':4,
+            'Read Input Registers':4,
             'Force Single Coil':5,
             'Preset Single Register':6,
+            'Loopback Diagnosis':8,
             'Force Multiple Coils':15,
             'Preset Multiple Registers':16,
             'Masked Write Register':22}
@@ -19,7 +20,8 @@ class Modbus_constants():
         self.WRITE_COILS = 15
         self.WRITE_REGISTERS = 16
         self.READ_HOLDING_REGISTERS = 3
-        self.READ_INTERNAL_HOLDING_REGISTERS = 4
+        self.READ_INPUT_HOLDING_REGISTERS = 4
         self.WRITE_REGISTER = 6
+        self.LOOPBACK_DIAGNOSIS = 8
         self.MASKED_REGISTER = 22
         super().__init__(self, *args, **kwargs)
