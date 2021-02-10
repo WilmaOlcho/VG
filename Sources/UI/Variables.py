4f80b22b2e92dbec2892e40de27ab27b799a2de3
WilmaOlcho <WilmaOlcho@gmail.com>
2021-02-10 14:57:52 +0100

    Creating widgetsettings file there is a trick to create callable object of a class
    a = ClassName(*args, **kwargs)
    a()
    
    It can be possible when class have __call__ method
    class ClassName(object):
        def __call__(self):
            return "Some result of callable object"

--- Sources/UI/Variables.py
+++ Sources/UI/Variables.py
@@ -1,6 +1,7 @@
 
 class Variables(dict):
-    def __init__(self):
+    def __init__(self, **widgetsettings):
+        self['widgetsettings'] = widgetsettings
         self.jsonpath = ''
         self.currentProgram = ''
         self.programposstart = 0
